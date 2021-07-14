2eceaa7b3630af21b4d02cd08fb65f23bbe98a67
Sabrina Sides <sabrinasides91@gmail.com>
2021-07-13 21:10:36 -0500

    organizing app, adding component files, base landing and home page

---
+++ src/components/HomePage.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <header role="banner">
+            <h1>Git It</h1>
+            <h2>Find What Feels Like You</h2>
+            <p>[<em>Tshirt pic in background</em>]</p>
+        </header>
+
+        <h3>Shop The Products:</h3>
+        <section>
+            See all the t-shirts
+            <p>[<em>placeholder for screenshot of coding tshirt</em>]</p>
+        </section>
+
+        <section>
+            See Men's t-shirts
+            <p>[<em>placeholder for screenshot of coding tshirt</em>]</p>
+        </section>
+
+        <section>
+            See Women's t-shirts
+            <p>[<em>placeholder for screenshot of coding tshirt</em>]</p>
+        </section>
+
+        <section>
+          About our Shirts:
+           - icons with info on material
+           - puns hand chosen by programmers
+           - socially responsible production
+          </section>
+            </div>
+        )
+    }
+}
+
+export default HomePage
---
+++ src/components/LandingPage.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+
+class LandingPage extends Component {
+  render(){
+    return (
+        <div>
+            <header role="banner">
+        <h1>Git It</h1>
+        <h2>A Shop for Coding Merch</h2>
+      </header>
+      <section>
+        <header>
+            <h3>Wear Your Coding Nerdiness with Pride</h3>
+        </header>
+        <p>[<em>placeholder for screenshot of coding tshirt</em>]</p>
+        <p>Info about how GitIt sells coding related merch</p>
+      </section>
+      <section>
+        <header>
+            <h3>Gifts for Family and Friends</h3>
+        </header>
+        <p>[<em>placeholder for screenshot of of coding tshirt</em>]</p>
+        <p>Get the perfect gift for the programmer in your life</p>
+      </section>
+      <section>
+        <header>
+            <h3>Look Great Even If You Don't Leave The House</h3>
+        </header>
+        <p>[<em>placeholder for screenshot of tshirt</em>]</p>
+        <p>Variety of clever puns to choose from</p>
+      </section>
+      <section>
+        <header>
+            <h3>Start Shopping Now</h3>
+        </header>
+        <button onClick={() => this.props.history.push('/shop')}>Shop</button>
+      </section>
+        </div>
+    )
+  }
+}
+
+export default LandingPage
